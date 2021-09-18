f642f5d7dbe6b2f03abcdb06c9d1b6ee6949146c
Jaareet <80985233+Jaareet@users.noreply.github.com>
2021-09-18 21:52:07 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Fivem chat by canariodev#0001'
+
+client_scripts {
+    'cl_chat.lua'
+}
+server_scripts {
+    'sv_chat.lua'
+}
+
+files {
+    '**/**/**/**/**/*.*'
+}
+
+ui_page {
+    'ui/chat.html'
+}
